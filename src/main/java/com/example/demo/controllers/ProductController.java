bc69baa2c38e379ad871669d9a2dc1169ca9c1eb
adehndr <56023937+adehndr@users.noreply.github.com>
2022-08-11 22:55:26 +0700

    implementing validation

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -1,6 +1,16 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.ResponseData;
 import com.example.demo.models.entities.Product;
 import com.example.demo.services.ProductService;
 
@@ -21,8 +32,21 @@ public class ProductController {
     private ProductService productService;
 
     @PostMapping
-    public Product create(@RequestBody Product product){
-        return productService.create(product);
+    public ResponseEntity<ResponseData<Product>> create(@RequestBody @Valid Product product, Errors errors){
+        ResponseData<Product> responseData = new ResponseData<Product>();
+        if (errors.hasErrors()) {
+            List<String> errorList = new ArrayList<String>();
+            for (ObjectError iterable_element : errors.getAllErrors()) {
+                errorList.add(iterable_element.getDefaultMessage());
+            }
+            responseData.setMessages(errorList);
+            responseData.setStatus(false);
+            responseData.setPayload(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        responseData.setStatus(true);
+        responseData.setPayload(productService.create(product));
+        return ResponseEntity.status(HttpStatus.OK).body(responseData);
     }
 
     @GetMapping
@@ -36,8 +60,21 @@ public class ProductController {
     }
 
     @PutMapping
-    public Product update(@RequestBody Product product){
-        return productService.create(product);
+    public ResponseEntity<ResponseData<Product>> update(@RequestBody @Valid Product product, Errors errors){
+        ResponseData<Product> responseData = new ResponseData<Product>();
+        if (errors.hasErrors()) {
+            List<String> errorList = new ArrayList<String>();
+            for (ObjectError iterable_element : errors.getAllErrors()) {
+                errorList.add(iterable_element.getDefaultMessage());
+            }
+            responseData.setMessages(errorList);
+            responseData.setStatus(false);
+            responseData.setPayload(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        responseData.setStatus(true);
+        responseData.setPayload(productService.create(product));
+        return ResponseEntity.status(HttpStatus.OK).body(responseData);
     }
 
     @DeleteMapping("/{id}")
---
+++ src/main/java/com/example/demo/dto/ResponseData.java
@@ -0,0 +1,31 @@
+package com.example.demo.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResponseData<T> {
+    private Boolean status;
+    private List<String> messages = new ArrayList<String>();
+    private T payload;
+    
+    public Boolean getStatus() {
+        return status;
+    }
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+    public List<String> getMessages() {
+        return messages;
+    }
+    public void setMessages(List<String> messages) {
+        this.messages = messages;
+    }
+    public T getPayload() {
+        return payload;
+    }
+    public void setPayload(T payload) {
+        this.payload = payload;
+    }
+
+    
+}

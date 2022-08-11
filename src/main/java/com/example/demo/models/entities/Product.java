814b3d095cec649fe98712db634d5810697a2f3f
adehndr <56023937+adehndr@users.noreply.github.com>
2022-08-11 23:34:42 +0700

    create new entity for category and supplier

--- src/main/java/com/example/demo/models/entities/Product.java
+++ src/main/java/com/example/demo/models/entities/Product.java
@@ -1,12 +1,17 @@
 package com.example.demo.models.entities;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
@@ -29,6 +34,15 @@ public class Product implements Serializable {
     private String description;
 
     private Double price;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToMany
+    @JoinTable(name = "tbl_product_supplier",
+    joinColumns = @JoinColumn(name = "product_id"),
+    inverseJoinColumns = @JoinColumn(name = "supplier_id"))
+    private Set<Supplier> suppliers;
 
     public Product() {
     }
@@ -71,6 +85,27 @@ public class Product implements Serializable {
     public void setPrice(Double price) {
         this.price = price;
     }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Set<Supplier> getSuppliers() {
+        return suppliers;
+    }
+
+    public void setSuppliers(Set<Supplier> suppliers) {
+        this.suppliers = suppliers;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
 
     
 }
